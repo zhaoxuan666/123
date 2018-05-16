6547c35cb5f00dcca6445e147a3e103b442d9066
zhaoxuan666 <35882296+zhaoxuan666@users.noreply.github.com>
2018-05-16 20:57:32 +0800

    冒泡排序
    冒泡排序

---
+++ 冒泡排序/冒泡排序/sort.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <Windows.h>
+void Swap(int *a,int*b)
+{
+	int tmp=*a;
+	*a=*b;
+	*b=tmp;
+}
+void sort(int *arr,int len)
+{
+	for (int i=0;i<len;i++)
+	{
+		int flag=0;
+		for (int j=0;j<len-i-1;j++)
+		{
+			if (arr[j]>arr[j+1])
+			{
+				flag=1;
+				Swap(&arr[j],&arr[j+1]);
+			}
+		}
+		if (flag==0)
+		{
+			return;
+		}
+	}
+}
+int main()
+{
+	int arr[]={9,8,7,6,5,4,3,2,1};
+	int len=sizeof(arr)/sizeof(arr[0]);
+	sort(arr,len);
+	for (int i=0;i<len;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	system("pause");
+}
