5ae04c0cd0f55409709f054b3caa079c4e6f195e
zhaoxuan666 <35882296+zhaoxuan666@users.noreply.github.com>
2018-05-20 23:03:46 +0800

    单链表
    单链表

---
+++ 单链表的实现/单链表的实现/1.h
@@ -0,0 +1,25 @@
+#ifndef __1_H__
+#define __1_H__
+#include <stdio.h>
+#include <Windows.h>
+
+typedef struct List
+{
+	int date;
+	struct List *next;
+}List;
+
+
+typedef int DataType; 
+
+typedef struct SListNode 
+{ 
+	struct SListNode* next; 
+	DataType data; 
+}SListNode; 
+
+
+//List* Buynode(int x);
+
+
+#endif
